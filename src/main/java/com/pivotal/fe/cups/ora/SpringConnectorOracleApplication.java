b4477fd756686214541bc8067a8c409d260353db
pivotal <pivotal@localhost>
2015-10-20 15:25:39 -0700

    do it

--- src/main/java/com/pivotal/fe/cups/ora/SpringConnectorOracleApplication.java
+++ src/main/java/com/pivotal/fe/cups/ora/SpringConnectorOracleApplication.java
@@ -1,12 +1,18 @@
 package com.pivotal.fe.cups.ora;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringConnectorOracleApplication {
 	
-	@Autowired JdbcTemplate jdbcTemplate;
+//	@Autowired
+	JdbcTemplate jdbcTemplate = null;
+
+	@Autowired
+	@Qualifier("mysqlDS")
+	DataSource mysqlDSUPS = null;
 	
 	@RequestMapping("/")
 	public Map<String,String> home()
@@ -28,11 +39,30 @@ public class SpringConnectorOracleApplication {
 		else
 		{
 			retVal.put("jdbcTemplate", "Exists!");
-			SqlRowSet rowSet = jdbcTemplate.queryForRowSet("select * from wells_test");
+			SqlRowSet rowSet = jdbcTemplate.queryForRowSet("select count(*) from xf_thread");
 			int c = 0;
 			while(rowSet.next())
 			{
 				retVal.put("row"+(c++), rowSet.getString(1));
+			}
+		}
+
+		if(mysqlDSUPS == null) { retVal.put("mysqlDS", "is null :-("); }
+		else
+		{
+			retVal.put("mysqlDS", "exists :-)");
+			try {
+				Connection conn = mysqlDSUPS.getConnection();
+				Statement s = conn.createStatement();
+				ResultSet rowSet = s.executeQuery("select count(*) from xf_thread");
+				int c = 0;
+				while(rowSet.next())
+				{
+					retVal.put("row"+(c++), rowSet.getString(1));
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 
